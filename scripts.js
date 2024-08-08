24b26465e3e03c15f205f4e2c52aa1889691fe98
Rbeard33 <Riley.beard20@gmail.com>
2024-08-08 09:38:15 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for signing up! You will receive your discount code shortly.');
+    // Here you would typically send the data to your server
+});
